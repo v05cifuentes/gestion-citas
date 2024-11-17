f1cc2076aae884bcf4c27e7ded3426559841ae32
Viviana Cifuentes <viviana.cifuentes@software.com>
2024-11-16 20:37:03 -0500

    Add user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { validateUserGuard } from './guard/validateuser.guard';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
 
@@ -12,6 +13,7 @@ const routes: Routes = [
     path: '', component: NavbarComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent},
+      { path: 'user', component: UserComponent},
       { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     ]
   },
